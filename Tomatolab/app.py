b32dee33a85db7d8e28292b63afc01cb51cc3f7b
LemonTea-lab20 <c2006p8@gmail.com>
2025-12-10 08:56:23 +0900

    Update app.py

--- Tomatolab/app.py
+++ Tomatolab/app.py
@@ -223,14 +223,14 @@ html_template = """
     <!-- 粒子 -->
     <script>
         const CONFIG = {
-            particleSize: 4,
+            particleSize: 6,
             particleMargin: 1,
             repulsionRadius: 80,
             repulsionForce: 3.0,
             friction: 0.12,
             returnSpeed: 0.02,
-            samplingStep: 2,
-            maxDisplayRatio: 0.65
+            samplingStep: 4,
+            maxDisplayRatio: 0.60
         };
         let particles = [], mouse = { x: -1000, y: -1000 };
         const canvas = document.getElementById('canvas'), ctx = canvas.getContext('2d');
